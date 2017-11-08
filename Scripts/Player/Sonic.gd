8d47ecc1a2de5159dd827b87cea27e5866391463
Stuart "Sslaxx" Moore <stuart@sslaxx.co.uk>
2017-11-08 21:25:32 +0000

    * Started to comment the code. * Added a bit more to the proof-of-concept HUD layer.
    Signed-off-by: Stuart "Sslaxx" Moore <stuart@sslaxx.co.uk>

--- Scripts/Player/Sonic.gd
+++ Scripts/Player/Sonic.gd
@@ -5,6 +5,7 @@ var dir = 0
 var speed = 0
 var velocity = 0
 var breaktime = 0
+onready var sprite_anim = get_node ("AnimatedSprite")
 const ACC = 0.046875
 const DEC = 0.5
 const FRIC = ACC
@@ -12,13 +13,13 @@ const TOP = 6
 
 func _ready():
 	set_fixed_process (true)
-	var sprite_anim = get_node ("AnimatedSprite")
 	return
 
 func _fixed_process (delta):
-	if (xdir != 0):
-		dir = xdir
+	if (xdir != 0):	# If the direction isn't 0, make sure it's consistent.
+		dir = xdir	# dir is used to calculate the direction of movement, xdir is for changing it.
 
+	# Direction is -1 if Sonic is moving left, 1 if right, and 0 otherwise.
 	if (Input.is_action_pressed ("move_left")):
 		xdir = -1
 	elif (Input.is_action_pressed ("move_right")):
@@ -28,13 +29,13 @@ func _fixed_process (delta):
 
 	if (xdir):
 		if (speed < TOP):
-			speed += ACC
+			speed += ACC	# Speed Sonic up until he is at top speed.
 	else:
 		if (speed > 0):
-			speed -= FRIC
+			speed -= FRIC	# Slow Sonic down according to the friction rating.
 
-	velocity = (speed * dir)
-	move (Vector2 (velocity, 0))
+	velocity = (speed * dir)		# Set the velocity for Sonic's movement.
+	move (Vector2 (velocity, 0))	# And move Sonic appropriately.
 
 	return
 
