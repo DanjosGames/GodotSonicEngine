63ce2d9529f4c5d25429b8842c615bac1f4f0ac0
Stuart "Sslaxx" Moore <stuart@sslaxx.demon.co.uk>
2017-11-08 19:15:31 +0000

    Cleaned up some of the code, separated Sonic to his own discrete scene file. Some degree of organisation will be required down the line.

--- Scripts/Player/Sonic.gd
+++ Scripts/Player/Sonic.gd
@@ -11,26 +11,29 @@ const FRIC = ACC
 const TOP = 6
 
 func _ready():
-	set_fixed_process(true)
-	var sprite_anim = get_node("AnimatedSprite")
+	set_fixed_process (true)
+	var sprite_anim = get_node ("AnimatedSprite")
 
-func _fixed_process(delta):
-	if xdir:
+func _fixed_process (delta):
+	if (xdir != 0):
 		dir = xdir
 	
-	if Input.is_action_pressed("move_left"):
+	if (Input.is_action_pressed("move_left")):
 		xdir = -1
-	elif Input.is_action_pressed("move_right"):
+	elif (Input.is_action_pressed("move_right")):
 		xdir = 1
 	else:
 		xdir = 0
 	
-	if xdir:
-		if speed < TOP:
+	if (xdir):
+		if (speed < TOP):
 			speed += ACC
 	else:
-		if speed > 0:
+		if (speed > 0):
 			speed -= FRIC
 	
-	velocity = speed * dir
-	move(Vector2(velocity, 0))
+	velocity = (speed * dir)
+	move (Vector2 (velocity, 0))
+	
+	return
+	
