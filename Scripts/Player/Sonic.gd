be3cda2d54fb0b6834ae1065769a1c6719a7b3c7
Cepukka <17429618+Cepukka@users.noreply.github.com>
2017-11-08 20:02:29 +0200

    All Files

---
+++ Scripts/Player/Sonic.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+var xdir = 0
+var dir = 0
+var speed = 0
+var velocity = 0
+var breaktime = 0
+const ACC = 0.046875
+const DEC = 0.5
+const FRIC = ACC
+const TOP = 6
+
+func _ready():
+	set_fixed_process(true)
+	var sprite_anim = get_node("AnimatedSprite")
+
+func _fixed_process(delta):
+	if xdir:
+		dir = xdir
+	
+	if Input.is_action_pressed("move_left"):
+		xdir = -1
+		sprite_anim.set_flip_h(true)
+	elif Input.is_action_pressed("move_right"):
+		xdir = 1
+	else:
+		xdir = 0
+	
+	if xdir:
+		if speed < TOP:
+			speed += ACC
+	else:
+		if speed > 0:
+			speed -= FRIC
+	
+	velocity = speed * dir
+	move(Vector2(velocity, 0))
