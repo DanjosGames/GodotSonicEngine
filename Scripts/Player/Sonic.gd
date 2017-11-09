c843a5f9c24e648e8a1c9283c2abab4298925456
Stuart "Sslaxx" Moore <stuart@sslaxx.co.uk>
2017-11-09 07:26:14 +0000

    A bit more animation functionality!
    Added a basic function for changing the sprite's animation.
    
    Signed-off-by: Stuart "Sslaxx" Moore <stuart@sslaxx.co.uk>

--- Scripts/Player/Sonic.gd
+++ Scripts/Player/Sonic.gd
@@ -6,11 +6,14 @@ var speed = 0
 var velocity = 0
 var breaktime = 0
 
-export(int) var Rings = 0					# Number of rings the player has.
-export(int) var Lives = 3					# Lives left.
-export(int) var Score = 0					# Score.
+export(int) var Rings = 0	# Number of rings the player has.
+export(int) var Lives = 3	# Lives left.
+export(int) var Score = 0	# Score.
 
-onready var sprite_anim = get_node ("AnimatedSprite")
+# Set up sprite_anim and sprite_anim_node. The _node variable points to the animation node, of course.
+# sprite_anim contains the animation currently playing.
+onready var sprite_anim_node = get_node ("AnimatedSprite")
+var sprite_anim = ""
 
 const ACCEL_RATE = 0.046875
 const DECEL_RATE = 0.5
@@ -19,8 +22,21 @@ const TOP_X_SPEED = 6
 
 func _ready():
 	set_fixed_process (true)
+	sprite_anim = sprite_anim_node.get_animation()	# Make sure sprite_anim contains the default animation value on ready.
 	print ("Sonic entered the world at ", get_pos ())
 	return
+
+# change_anim
+# func change_anim (new_anim)
+# Changes the currently playing animation to one specified (by new_anim). Won't change the animation if it's already playing.
+# Returns true if the animation has changed, false otherwise.
+# TODO: Maybe make this function able to change direction animation plays in, etc.?
+func change_anim (new_anim):
+	if (new_anim != sprite_anim):	# This is a new animation...
+		sprite_anim = new_anim		# ...so set sprite_anim to new_anim.
+		sprite_anim_node.set_animation (sprite_anim)	# And make the sprite animation node play the new animation.
+		return (true)
+	return (false)
 
 func _fixed_process (delta):
 	if (xdir != 0):	# If the direction isn't 0, make sure it's consistent.
@@ -41,7 +57,7 @@ func _fixed_process (delta):
 		if (speed > 0):
 			speed -= FRICTION	# Slow Sonic down according to the friction rating.
 
-	velocity = (speed * dir)		# Set the velocity for Sonic's movement (sign dir being used to signify direction).
+	velocity = (speed * dir)		# Set the velocity for Sonic's movement (dir being used to signify direction).
 	move (Vector2 (velocity, 0))	# And move Sonic appropriately.
 
 	return
