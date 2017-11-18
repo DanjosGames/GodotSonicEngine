42e1e9161158559eb76753b5e0bd8be2246aabb8
Stuart "Sslaxx" Moore <stuart@sslaxx.co.uk>
2017-11-18 11:37:03 +0000

    Vector2s instead of separate x and y vars! For vars and consts.
    So it's not x_speed or something like that, it's speed.x instead.
    
    Signed-off-by: Stuart "Sslaxx" Moore <stuart@sslaxx.co.uk>

--- Scripts/Player/Sonic.gd
+++ Scripts/Player/Sonic.gd
@@ -1,31 +1,30 @@
 extends KinematicBody2D
 
-var xdir = 0
-var dir = 0
-var speed = 0
-var velocity = 0
+var move_dir = Vector2 (0, 0)
+var dir_sign = Vector2 (0, 0)
+var speed = Vector2 (0, 0)
+var velocity = Vector2 (0, 0)
 var break_time = 0
 var anim_speed = 0
 
-export(int) var rings = 0	# Number of rings the player has.
-export(int) var lives = 3	# Lives left.
-export(int) var score = 0	# Score.
+export(int) var rings = 0		# Number of rings the player has.
+export(int) var lives = 3		# Lives left.
+export(int) var score = 0		# Score.
+export(Vector2) var checkpoint_pos	# Where was the last checkpoint? Make sure to set this to (0, 0) when on a new level etc.
 
 # Set up sprite_anim and sprite_anim_node. The _node variable points to the animation node, of course.
 # sprite_anim contains the animation currently playing.
 onready var sprite_anim_node = get_node ("AnimatedSprite")
-var sprite_anim = ""	# This value is set in _ready because I'm not sure you can guarantee the order "onready var"s execute in.
-var sprite_anim_frames	# Ditto this one.
+onready var sprite_anim = sprite_anim_node.get_animation ()	# Make sure sprite_anim contains the default animation value.
+onready var sprite_anim_frames = sprite_anim_node.get_sprite_frames ()
 
 const ACCEL_RATE = 0.046875
 const DECEL_RATE = 0.5
 const FRICTION = ACCEL_RATE
-const TOP_X_SPEED = 6
+const TOP_SPEED = Vector2 (6, 6)
 
 func _ready():
 	print ("Sonic entered the world at ", get_pos ())
-	sprite_anim = sprite_anim_node.get_animation()	# Make sure sprite_anim contains the default animation value on ready.
-	sprite_anim_frames = sprite_anim_node.get_sprite_frames ()
 	set_fixed_process (true)
 	return
 
@@ -44,43 +43,44 @@ func change_anim (new_anim):
 	return (false)
 
 func _fixed_process (delta):
-	if (xdir != 0):	# If the direction isn't 0, make sure it's consistent.
-		dir = xdir	# dir is used to calculate the direction of movement, xdir is for changing it.
+	if (dir_sign.x != 0):	# If the direction isn't 0, make sure it's consistent.
+		move_dir.x = dir_sign.x	# move_dir.x is used to calculate the direction of movement in the x axis; dir_sign.x is for changing it.
 
-	# Direction is -1 if Sonic is moving left, 1 if right, and 0 otherwise.
+	# Direction is -1 if Sonic is moving left/up, 1 if right/down, and 0 otherwise.
 	# Can only move in one direction at a time (so pressing left while holding down right won't work)!
-	if (Input.is_action_pressed ("move_left") && xdir != 1):
-		xdir = -1
+	if (Input.is_action_pressed ("move_left") && dir_sign.x != 1):
+		dir_sign.x = -1
 		sprite_anim_node.set_flip_h (true)
-	elif (Input.is_action_pressed ("move_right") && xdir != -1):
-		xdir = 1
+	elif (Input.is_action_pressed ("move_right") && dir_sign.x != -1):
+		dir_sign.x = 1
 		sprite_anim_node.set_flip_h (false)
 	else:
-		xdir = 0
+		dir_sign.x = 0
 
 	if (Input.is_action_pressed ("move_jump")):
 		print ("AAAA")
 
-	if (xdir):
-		if (speed < TOP_X_SPEED):
-			speed += ACCEL_RATE	# Speed Sonic up until he is at top speed.
+	if (dir_sign.x):
+		if (speed.x < TOP_SPEED.x):
+			speed.x += ACCEL_RATE	# Speed Sonic up until he is at top speed.
 	else:
-		if (speed > 0):
-			speed -= FRICTION	# Slow Sonic down according to the friction rating.
+		if (speed.x > 0):
+			speed.x -= FRICTION	# Slow Sonic down according to the friction rating.
 
 	#AnimationChange
-#	anim_speed = max(4-(abs(speed)), 1)
+#	anim_speed = max(4-(abs(speed.x)), 1)
 	
-	if (speed == 0):
-		change_anim ("Idle")
+	if (speed.x > 0 && speed.x < 3):
+		change_anim ("Walk")
+	elif (speed.x >= 3):
+		change_anim ("Jog")
 	else:
-		if (speed <= 3):
-			change_anim ("Walk")
-		elif (speed >= 3):
-			change_anim ("Jog")
+		change_anim ("Idle")	# This is the default animation.
 
-	velocity = (speed * dir)		# Set the velocity for Sonic's movement (dir being used to signify direction).
-	move (Vector2 (velocity, 0))	# And move Sonic appropriately.
+	### KEEP THESE AT THE BOTTOM OF THE FUNCTION, THESE ACTUALLY DO THE MOVEMENT AFTER EVERYTHING ELSE IS PROCESSED AND CALCULATED.
+	velocity.x = (speed.x * move_dir.x)	# Set the velocity for Sonic's movement on the x axis.
+	velocity.y = (speed.y * move_dir.y)	# Ditto for the y axis.
+	move (Vector2 (velocity.x, velocity.y))	# And move Sonic appropriately.
 
 	return
 
