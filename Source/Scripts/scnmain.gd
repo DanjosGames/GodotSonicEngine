d93e8b67bbee1403bea9e233d48db907bbc15227
Stuart "Sslaxx" Moore <stuart@sslaxx.co.uk>
2017-12-16 15:21:14 +0000

    Windows boxen...
    Making sure the Windows boxen is up-to-date, and dealing with its own
    little issues.
    
    Signed-off-by: Stuart "Sslaxx" Moore <stuart@sslaxx.co.uk>

--- Source/Scripts/scnmain.gd
+++ Source/Scripts/scnmain.gd
@@ -8,7 +8,7 @@ func _ready ():
 
 # If any key is pressed, start the game.
 # TODO: Something a bit more refined than just restarting the whole program, but for now it'll do.
-# TODO: Restarting the game from level_test.tscn doesn't restart from here (because this scene has been unloaded).
+# TODO: Restarting the game from level_test.tscn doesn't restart from here (because this scene will have been unloaded).
 func _unhandled_key_input (event):
 	if (event.pressed):
 		game_space.reset_values ()
