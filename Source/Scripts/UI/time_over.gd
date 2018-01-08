c05eeab2ea1a5858899582fbe736d385988a38c4
Stuart "Sslaxx" Moore <stuart@sslaxx.co.uk>
2018-01-08 11:04:48 +0000

    Inconsistency found?
    As per https://github.com/godotengine/godot/issues/15218
    
    Maybe. The queue_free line is somewhere different (and not the last line
    of the block it's in either). Now, this indicates that perhaps the Windows
    behaviour is correct and the Linux behaviour incorrect, but they're still
    not consistent (should be the same on both platforms, should it not?).
    
    Signed-off-by: Stuart "Sslaxx" Moore <stuart@sslaxx.co.uk>

--- Source/Scripts/UI/time_over.gd
+++ Source/Scripts/UI/time_over.gd
@@ -1,17 +1,20 @@
+### Deals with time over. Very similar to game over, really, just that this doesn't reset things for a new game.
+
 extends Sprite
 
-func _ready():
+func _ready ():
 	if (has_node ("/root/Level/Music_Player")):
 		$"/root/Level/Music_Player".stop ()	# Stop whatever other music is playing, and play the game over music instead.
 		music_player.play_music ("res://Assets/Audio/Music/63_-_Game_Over.ogg")
 	get_tree ().set_pause (true)		# Pause the game in the background.
 	return
 
-func _unhandled_key_input(event):
+func _unhandled_key_input (event):
 	if (event.pressed):
 		music_player.stop_music ()
 		$"/root/Level/Music_Player".play ()
 		$"/root/Level/hud_layer".set ("layer", 32)
+		# FIXME? Is the next line below in the wrong place? Probably.
 		queue_free ()	# As this is queued, it'd be better put here than before reloading the main scene again.
 		game_space.player_character.set ("visible", true)
 		get_tree ().set_pause (false)		# Pause the game in the background.
