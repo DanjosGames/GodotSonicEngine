de5fe936387066f3116475d9727873c9df3da2e3
Stuart "Sslaxx" Moore <stuart@sslaxx.co.uk>
2018-01-28 14:02:45 +0000

    Space, saving...
    ...starting off by getting rid of larger files wherever possible.
    
    Signed-off-by: Stuart "Sslaxx" Moore <stuart@sslaxx.co.uk>

--- Source/Scripts/Levels/All/level_test.gd
+++ Source/Scripts/Levels/All/level_test.gd
@@ -3,7 +3,7 @@
 extends "res://Scripts/Levels/All/level_generic.gd"
 
 func _ready ():
-	$"Music_Player".stream = load ("res://Assets/Audio/Music/43-Final_Dreadnought_2.ogg")
+	$"Music_Player".stream = load ("res://Assets/Audio/Music/Level_Test.ogg")
 	$"Music_Player".play ()
 	print ("Level ready to go!")
 	return
